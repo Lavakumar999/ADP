93f331121d6f8769ab2470875a7536623171aea5
Lavakumar <lavakumarfire999@gmail.com>
2018-08-10 03:54:28 -0400

    Update register.component.ts

--- app/register/register.component.ts
+++ app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import{FormsModule,FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RegistrationValidator } from '../register/register.validator';
 
 @Component({
   selector: 'app-register',
@@ -8,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  passwordFormGroup: FormGroup;
+  RegisterForm:FormGroup;
+  submitted=false;
+  
+  constructor(private formBuilder:FormBuilder) {
+   }
 
   ngOnInit() {
+    this.passwordFormGroup = this.formBuilder.group({
+      password: ['', Validators.required],
+      cPassword: ['', Validators.required]
+    }, {
+      validator: RegistrationValidator.validate.bind(this)
+    });
+    this.RegisterForm=this.formBuilder.group({
+      fname:['',[Validators.required,Validators.pattern('^[a-zA-z]+$')]],
+      lname:['',[Validators.required,Validators.pattern('^[a-zA-z]+$')]],
+      dob:['',Validators.required],
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.minLength(6),Validators.maxLength(20),Validators.pattern('^[a-zA-z]+$')]],
+      passwordFormGroup: this.passwordFormGroup
+    });
+   
+  }
+  get f(){
+    return this.RegisterForm.controls;
+  }
+  onSubmit(){
+    this.submitted=true;
+    if(this.RegisterForm.invalid){
+      return
+    }
+    alert("Sucess")
   }
 
 }
